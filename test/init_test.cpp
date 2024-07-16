bc62210d2313b522a7e672f5fda715418645a100
Alex Do <alexxdo2004@gmail.com>
2024-07-16 21:13:05 +0100

    test: added example test

---
+++ test/init_test.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+TEST(init_test, init_test) {
+    ASSERT_EQ(1, 1);
+}
+
+
+int main(int argc, char **argv) 
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
