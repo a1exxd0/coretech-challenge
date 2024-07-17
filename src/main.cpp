5944aeb81c95517cdeae1d3cea84ad878e1c4dd3
Alex Do <alexxdo2004@gmail.com>
2024-07-17 22:00:43 +0100

    feat: finished task (badly :o)

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,62 @@
 #include <pcapplusplus/IPv4Layer.h>
 #include <pcapplusplus/Packet.h>
 #include <pcapplusplus/PcapFileDevice.h>
+#include "organise.cpp"
+
+// In terms of data I'll need to extract:
+// - Average packet size
+// - Total volume of data transmitted
+// - Most frequent destination IP
+// - Total number of packets broken down by transport layer protocol
+// - Any additional insights or anomalies
+
+/**
+ * Helper data:
+ * - total number of packets
+ * - map of dest ip to quantity or something
+ * - logger for transport layer types
+ */
+
 
 int main() {
-    pcpp::PcapFileReaderDevice reader("../../testfiles/packet-storm.pcap");
-    if (!reader.open())
-    {
-        std::cerr << "Error opening the pcap file" << std::endl;
-        return 1;
+    std::ios::sync_with_stdio(false); 
+    std::cin.tie(nullptr); 
+    std::cout.tie(nullptr);
+    std::cerr.tie(nullptr);
+    
+    std::string input_file = "../testfiles/packet-storm.pcap";
+    organise o(input_file);
+
+    std::ofstream nullStream("/dev/null");
+    std::streambuf* coutBuf = std::cout.rdbuf();
+    std::cout.rdbuf(nullStream.rdbuf());
+
+    std::streambuf* cerrBuf = std::cerr.rdbuf();
+    std::cerr.rdbuf(nullStream.rdbuf());
+
+    while (o.read_packet());
+
+    std::cout.rdbuf(coutBuf);
+    std::cerr.rdbuf(cerrBuf);
+
+    const organise::address_map &m = o.getAddressMap();
+    pcpp::IPv4Address mostCommon;
+    size_t occurences = 0;
+    for (const auto &[addr, q]: m) {
+        if (q > occurences) {
+            mostCommon = addr;
+            occurences = q;
+        }
     }
 
-    // read the first (and only) packet from the file
-    pcpp::RawPacket rawPacket;
-    if (!reader.getNextPacket(rawPacket))
-    {
-        std::cerr << "Couldn't read the first packet in the file" << std::endl;
-        return 1;
-    }
-
-    // parse the raw packet into a parsed packet
-    pcpp::Packet parsedPacket(&rawPacket);
-
-    // verify the packet is IPv4
-    if (parsedPacket.isPacketOfType(pcpp::IPv4))
-    {
-        // extract source and dest IPs
-        pcpp::IPv4Address srcIP = parsedPacket.getLayerOfType<pcpp::IPv4Layer>()->getSrcIPv4Address();
-        pcpp::IPv4Address destIP = parsedPacket.getLayerOfType<pcpp::IPv4Layer>()->getDstIPv4Address();
-
-        // print source and dest IPs
-        std::cout
-          << "Source IP is '" << srcIP << "'; "
-          << "Dest IP is '" << destIP << "'"
-          << std::endl;
-    }
-
-    // close the file
-    reader.close();
-
+    double averagePacketSize = 
+        static_cast<double>(o.getTotalVolume()) / static_cast<double>(o.getTotalPackets());
+    double totalPayload = 
+        static_cast<double>(o.getTotalVolume()) / static_cast<double>(1024 * 1024);
+    std::cout << "Average packet size: " << averagePacketSize << " bytes.\n";
+    std::cout << "Total payload size: " << totalPayload << " megabytes\n";
+    std::cout << "Most frequent IP: " << mostCommon.toString() << "\n";
+    std::cout << "TCP-based packet count: " << o.getTCPCount() << "\n";
+    std::cout << "UDP-based packet count: " << o.getUDPCount() << "\n";
     return 0;
 }
---
+++ src/organise.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <unordered_map>
+#include <exception>
+#include <stdexcept>
+#include <pcapplusplus/IPv4Layer.h>
+#include <pcapplusplus/Packet.h>
+#include <pcapplusplus/PcapFileDevice.h>
+#include <pcapplusplus/PcapLiveDeviceList.h>
+#include <pcapplusplus/Layer.h>
+#include <pcapplusplus/TLVData.h>
+#include <pcapplusplus/EthLayer.h>
+#include <pcapplusplus/TcpLayer.h>
+#include <pcapplusplus/UdpLayer.h>
+#include <pcapplusplus/DnsLayer.h>
+#include <pcapplusplus/Logger.h>
+
+// In terms of data I'll need to extract:
+// - Average packet size
+// - Total volume of data transmitted
+// - Most frequent destination IP
+// - Total number of packets broken down by transport layer protocol
+// - Any additional insights or anomalies
+
+/**
+ * Helper data:
+ * - total number of packets
+ * - map of dest ip to quantity or something
+ * - logger for transport layer types
+ */
+namespace std {
+    template <>
+    struct hash<pcpp::IPv4Address> {
+        [[nodiscard]] inline std::size_t operator()(const pcpp::IPv4Address& ip) const noexcept {
+            return std::hash<uint32_t>{}(ip.toInt());
+        }
+    };
+}
+
+[[nodiscard]] inline bool operator==(const pcpp::IPv4Address &a1, const pcpp::IPv4Address &a2) {
+    return a1.toInt() == a2.toInt();
+}
+
+class organise {
+public:
+    using address_map = std::unordered_map<pcpp::IPv4Address, size_t>;
+    using protocol_map = std::unordered_map<pcpp::ProtocolType, size_t>;
+
+private:
+    pcpp::RawPacket _packet;
+    pcpp::PcapFileReaderDevice reader;
+    size_t total_packets;
+    size_t total_volume;
+    size_t suspicious_packets;
+    address_map _address_map;
+    protocol_map _protocol_map;
+
+public:
+
+    [[nodiscard]] size_t getTotalPackets() const noexcept {
+        return total_packets;
+    }
+
+    [[nodiscard]] size_t getTotalVolume() const noexcept {
+        return total_volume;
+    }
+
+    [[nodiscard]] size_t getSuspiciousPackets() const noexcept {
+        return suspicious_packets;
+    }
+
+    [[nodiscard]] const address_map& getAddressMap() const noexcept {
+        return _address_map;
+    }
+
+    [[nodiscard]] const protocol_map& getProtocolMap() const noexcept {
+        return _protocol_map;
+    }
+
+    [[nodiscard]] size_t getTCPCount() noexcept {
+        return _protocol_map[pcpp::TCP];
+    }
+
+    [[nodiscard]] size_t getUDPCount() noexcept {
+        return _protocol_map[pcpp::UDP];
+    }
+
+    organise(std::string packet_path) : 
+        reader(packet_path),
+        total_packets(0),
+        total_volume(0),
+        suspicious_packets(0),
+        _address_map(),
+        _protocol_map() 
+    {
+        if (!reader.open()) {
+            std::cerr << "Error opening the pcap file" << std::endl;
+            throw std::ios_base::failure("Error opening the pcap file");
+        }
+    }
+
+    ~organise() {
+        reader.close();
+    }
+
+    bool read_packet() {
+        if (!reader.getNextPacket(_packet)) {
+            return false;
+        }
+
+        pcpp::Packet parsed(&_packet);
+
+        bool is_ipv4 = parsed.isPacketOfType(pcpp::IPv4);
+        bool is_tcp = parsed.isPacketOfType(pcpp::TCP);
+        bool is_udp = parsed.isPacketOfType(pcpp::UDP);
+
+        if (!is_ipv4) {
+            std::cerr << "Packet found is not IPv4\n";
+            return false;
+        }
+
+        if (!is_tcp && !is_udp) {
+            std::cerr << "Packet has unrecognised transport layer\n";
+            return false;
+        }
+
+        if (is_tcp && is_udp) {
+            std::cerr << "Detected both UDP and TCP layers\n";
+            return false;
+        }
+
+        ++total_packets;
+
+        pcpp::IPv4Address dest = parsed.getLayerOfType<pcpp::IPv4Layer>()->getDstIPv4Address();
+        _address_map[dest]++;
+
+        if (is_tcp) {
+            pcpp::TcpLayer* tcpLayer = parsed.getLayerOfType<pcpp::TcpLayer>();
+            _protocol_map[pcpp::TCP]++;
+            total_volume += tcpLayer->getLayerPayloadSize();
+        } else {
+            pcpp::UdpLayer* udpLayer = parsed.getLayerOfType<pcpp::UdpLayer>();
+            _protocol_map[pcpp::UDP]++;
+            total_volume += udpLayer->getLayerPayloadSize();
+        }
+
+        return true;
+    }
+
+};
